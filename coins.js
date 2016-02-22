a414bb7ffccc5cd253a5692edfaa9f3f5ed9fff0
Tim Maddux <timmaddux@Tims-MacBook-Pro.local>
2016-02-22 09:36:31 -0600

    Still not working. Think the logic is there, but I'm getting assistance.

--- coins.js
+++ coins.js
@@ -6,12 +6,15 @@
   };
   
 function coinCounter(coins) {
+  // if i take coins here, divide by .25, round down and then set that to equal quarters...
   (coins / .25).mathFloor = coinPurse.quarters;
-  ((coins - coinPurse.quarters * .25)  / .10).mathFloor = coinPurse.dimes;
-  (coins / .5).mathFloor = coinPurse.nickels;
-  (coins / .1).mathFloor = coinPurse.pennies;
+  // take the result of that, subtract from the # of coins remaining and then divide by 10...
+  ((coins - (coinPurse.quarters * .25))  / .10).mathFloor = coinPurse.dimes;
+  // take the result of that, subtract from the # of coins remaining and then divide by 5...
+  ((coins - (coinPurse.quarters * .25) - (coinPurse.dimes * .10)  / .5)).mathFloor = coinPurse.nickels;
+  // take the result of that, subtact from the # of coins remaining and then divide by 1...
+  (coins - (coinPurse.quarters * .25) - (coinPurse.dimes * .10) - (coinPurse.nickels * .05) / .01).mathFloor = coinPurse.pennies;
+  // and then ask it to return coinPurse... which I'm not sure I understand really....
   return coinPurse;
 };
-
-console.log("Meh?", coinCounter(.87));
 
